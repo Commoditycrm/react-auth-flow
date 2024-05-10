49a0d0f637896026d57bd105c36deba38459e5be
irfanhathoti <irfanhathoti@gmail.com>
2024-05-10 19:24:24 +0530

    added public and api folder

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { FirebaseUserController } from './controllers/main';
 import { BaseServer } from './server/base.server';
 
 // Initialize server with controllers
-const app = BaseServer.init([FirebaseUserController], '/api');
+export const app = BaseServer.init([FirebaseUserController], '/api');
 
 //TODO: setup logger, setup error handler
 // Start server
