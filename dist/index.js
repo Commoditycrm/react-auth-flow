cb92e89fe75294807cf12cc261fefeb84de140ef
Surafel Muluye <surafelmuluye@proton.me>
2024-05-14 13:06:50 +0300

    fix: export default server

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
 const main_1 = require("./controllers/main");
 const base_server_1 = require("./server/base.server");
 // Initialize server with controllers
-exports.app = base_server_1.BaseServer.init([main_1.FirebaseUserController], '/api');
+const app = base_server_1.BaseServer.init([main_1.FirebaseUserController], '/api');
 //TODO: setup logger, setup error handler
 // Start server
-base_server_1.BaseServer.start(exports.app, 3000);
+const server = base_server_1.BaseServer.start(app, 3000);
+exports.default = server;
